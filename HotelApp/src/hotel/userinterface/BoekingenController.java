8f00a689dd794d56dbd3461aa2bca240ede0c804
Grimeway <45391099+Grimeway@users.noreply.github.com>
2020-03-26 14:41:22 +0100

    Add files via upload

---
+++ HotelApp/src/hotel/userinterface/BoekingenController.java
@@ -0,0 +1,25 @@
+package hotel.userinterface;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class BoekingenController {
+    @FXML private Label berichtenLabel;
+    @FXML private TextField naam;
+    @FXML private TextField adres;
+    @FXML private DatePicker aankomstdatum;
+    @FXML private DatePicker vertrekdatum;
+    @FXML private ChoiceBox kamertype;
+
+    public void boek() {
+
+    }
+
+    public void reset() {
+
+    }
+
+}
